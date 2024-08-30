e9049d49e3bb6e2e004c43258a4363fae95af55f
Kikito <kylian.jozefiak@etu.unistra.fr>
2024-08-31 01:11:44 +0200

    kafka part b

--- kafka_log/main.go
+++ kafka_log/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"sync"
 
@@ -9,19 +11,17 @@ import (
 )
 
 type KafkaNode struct {
-	node             *maelstrom.Node
-	logs             map[string]map[int]interface{}
-	offsets          map[string]int
-	committedOffsets map[string]int
-	mu               sync.RWMutex
+	node *maelstrom.Node
+	kv   *maelstrom.KV
+	mu   sync.RWMutex
 }
 
 func NewKafkaNode() *KafkaNode {
+	node := maelstrom.NewNode()
+	kv := maelstrom.NewLinKV(node)
 	return &KafkaNode{
-		node:             maelstrom.NewNode(),
-		logs:             make(map[string]map[int]interface{}),
-		offsets:          make(map[string]int),
-		committedOffsets: make(map[string]int),
+		node: node,
+		kv:   kv,
 	}
 }
 
@@ -34,17 +34,42 @@ func (kn *KafkaNode) handleSend(msg maelstrom.Message) error {
 	key := body["key"].(string)
 	message := body["msg"]
 
+	offsetKey := fmt.Sprintf("%s_offset", key)
+	logKey := fmt.Sprintf("%s_log", key)
+
 	kn.mu.Lock()
 	defer kn.mu.Unlock()
 
-	if _, exists := kn.logs[key]; !exists {
-		kn.logs[key] = make(map[int]interface{})
-		kn.offsets[key] = 0
+	ctx := context.TODO()
+
+	var offset int
+	err := kn.kv.ReadInto(ctx, offsetKey, &offset)
+	if err != nil {
+		// Key doesn't exist, init with 0
+		if err := kn.kv.Write(ctx, offsetKey, 0); err != nil {
+			return err
+		}
+		offset = 0
 	}
 
-	offset := kn.offsets[key]
-	kn.logs[key][offset] = message
-	kn.offsets[key]++
+	// Read existing log
+	var log map[int]interface{}
+	err = kn.kv.ReadInto(ctx, logKey, &log)
+	if err != nil {
+		// Log doesn't exist, create a new one
+		log = make(map[int]interface{})
+	}
+
+	// append message to log
+	log[offset] = message
+	if err := kn.kv.Write(ctx, logKey, log); err != nil {
+		return err
+	}
+
+	// increment offset
+	if err := kn.kv.Write(ctx, offsetKey, offset+1); err != nil {
+		return err
+	}
 
 	return kn.node.Reply(msg, map[string]any{
 		"type":   "send_ok",
@@ -60,23 +85,39 @@ func (kn *KafkaNode) handlePoll(msg maelstrom.Message) error {
 
 	offsets := body["offsets"].(map[string]interface{})
 
-	kn.mu.RLock()
-	defer kn.mu.RUnlock()
-
 	result := make(map[string][][2]interface{})
 
 	for key, offsetInterface := range offsets {
-		offset := int(offsetInterface.(float64))
-		if log, exists := kn.logs[key]; exists {
-			messages := [][2]interface{}{}
-			for i := offset; i < kn.offsets[key]; i++ {
-				if msg, exists := log[i]; exists {
-					messages = append(messages, [2]interface{}{i, msg})
-				}
+		requestedOffset := int(offsetInterface.(float64))
+
+		offsetKey := fmt.Sprintf("%s_offset", key)
+		logKey := fmt.Sprintf("%s_log", key)
+
+		ctx := context.TODO()
+
+		var currentOffset int
+		err := kn.kv.ReadInto(ctx, offsetKey, &currentOffset)
+		if err != nil {
+			// If we can't read the offset, skip this key
+			continue
+		}
+
+		var log map[int]interface{}
+		err = kn.kv.ReadInto(ctx, logKey, &log)
+		if err != nil {
+			// If we can't read the log, skip this key
+			continue
+		}
+
+		messages := [][2]interface{}{}
+		for i := requestedOffset; i < currentOffset; i++ {
+			if msg, exists := log[i]; exists {
+				messages = append(messages, [2]interface{}{i, msg})
 			}
-			if len(messages) > 0 {
-				result[key] = messages
-			}
+		}
+
+		if len(messages) > 0 {
+			result[key] = messages
 		}
 	}
 
@@ -94,13 +135,45 @@ func (kn *KafkaNode) handleCommitOffsets(msg maelstrom.Message) error {
 
 	offsets := body["offsets"].(map[string]interface{})
 
-	kn.mu.Lock()
-	defer kn.mu.Unlock()
-
 	for key, offsetInterface := range offsets {
-		offset := int(offsetInterface.(float64))
-		if currentOffset, exists := kn.committedOffsets[key]; !exists || offset > currentOffset {
-			kn.committedOffsets[key] = offset
+		newOffset := int(offsetInterface.(float64))
+		commitKey := fmt.Sprintf("%s_commited", key)
+
+		for {
+			ctx := context.TODO()
+
+			var currentOffset int
+			err := kn.kv.ReadInto(ctx, commitKey, &currentOffset)
+			if err != nil {
+				// Key doesn't exist, try to create it
+				err := kn.kv.Write(ctx, commitKey, newOffset)
+				if err != nil {
+					// If write failed, it might be due to concurrent creation
+					// We'll retry the whole process
+					continue
+				}
+				break
+			}
+
+			if newOffset <= currentOffset {
+				// New offset is not greater than the current one, ignore
+				break
+			}
+
+			err = kn.kv.CompareAndSwap(ctx, commitKey, currentOffset, newOffset, false)
+			if err == nil {
+				// Successfully updated
+				break
+			}
+
+			// Check if it's a precondition failed error (i.e., concurrent modification)
+			if err.Error() == "precondition failed" {
+				// Retry the whole process
+				continue
+			}
+
+			// Some other error occurred
+			return err
 		}
 	}
 
@@ -117,14 +190,20 @@ func (kn *KafkaNode) handleListCommittedOffsets(msg maelstrom.Message) error {
 
 	keys := body["keys"].([]interface{})
 
-	kn.mu.RLock()
-	defer kn.mu.RUnlock()
-
 	offsets := make(map[string]int)
-	for _, key := range keys {
-		if offset, exists := kn.committedOffsets[key.(string)]; exists {
-			offsets[key.(string)] = offset
+	for _, keyInterface := range keys {
+		key := keyInterface.(string)
+		commitKey := fmt.Sprintf("%s_commited", key)
+
+		ctx := context.TODO()
+
+		var offset int
+		err := kn.kv.ReadInto(ctx, commitKey, &offset)
+		if err == nil {
+			offsets[key] = offset
 		}
+		// If err != nil, it means the key doesn't exist or there was a read error.
+		// In this case, we simply omit this key from the response, as per the original logic.
 	}
 
 	return kn.node.Reply(msg, map[string]any{
